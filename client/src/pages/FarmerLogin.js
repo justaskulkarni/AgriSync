61f622e111e83568ba4deb959477715f40614b61
AadiVj18 <vijayvargiya.meet@gmail.com>
2023-07-15 17:50:17 +0530

    Created A farmer login page and also added bootstrap

--- client/src/pages/FarmerLogin.js
+++ client/src/pages/FarmerLogin.js
@@ -1,42 +1,46 @@
-import React from 'react'
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../stylesheets/FarmerLogin.css";
 const Login = () => {
-    const [credentials, setCredentials] = useState({ email: "", password: "" });
-    const [error, setError] = useState(null);
-    let navigate = useNavigate();
-    const onChange = (event) => {
-		setCredentials({ ...credentials, [event.target.name]: event.target.value });
-	};
-    const handleSubmit = async (e) =>{
-        e.preventDefault();
-        const response = await fetch("http://localhost:6100/api/user/login", {
-			method: "POST",
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify({ email: credentials.email, password: credentials.password }),
-		});
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
+  const [error, setError] = useState(null);
+  let navigate = useNavigate();
+  const onChange = (event) => {
+    setCredentials({ ...credentials, [event.target.name]: event.target.value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:6100/api/user/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: credentials.email,
+        password: credentials.password,
+      }),
+    });
 
-		const json = await response.json();
+    const json = await response.json();
 
-		if (json.success) {
-			localStorage.setItem("Token", json.authToken);
-			navigate("/farmer/dashboard");
-		}
-
-		if (json.error) {
-			setError(json.error);
-			setCredentials({
-				email: "",
-				password: "",
-			});
-			setTimeout(() => {
-				setError(null);
-			}, 4000);
-		}
+    if (json.success) {
+      localStorage.setItem("Token", json.authToken);
+      navigate("/farmer/dashboard");
     }
-    return (
-        <>
-            <form onSubmit={handleSubmit} >
+
+    if (json.error) {
+      setError(json.error);
+      setCredentials({
+        email: "",
+        password: "",
+      });
+      setTimeout(() => {
+        setError(null);
+      }, 4000);
+    }
+  };
+  return (
+    <>
+      {/* <form onSubmit={handleSubmit} >
                 <label htmlFor="Email">Email</label>
                 <input type="email" value={credentials.email} name="email" onChange={onChange} placeholder="" />
 
@@ -45,9 +49,101 @@ const Login = () => {
 
                 <button>Login</button>
                 {error && <div>{error}</div>}
-            </form>
-        </>
-    )
-}
+            </form> */}
 
-export default Login
+      <section
+        className="h-100 gradient-form"
+        style={{ backgroundColor: "#eee" }}
+      >
+        <div className="container py-5 h-100">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col-xl-10">
+              <div className="card rounded-3 text-black">
+                <div className="row g-0">
+                  <div className="col-lg-6">
+                    <div className="card-body p-md-5 mx-md-4">
+                      <div className="text-center">
+                        <img
+                          src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
+                          style={{ width: 185 }}
+                          alt="logo"
+                        />
+                        <h4 className="mt-1 mb-5 pb-1">
+                          We are The Lotus Team
+                        </h4>
+                      </div>
+                      <form>
+                        <p>Please login to your account</p>
+                        <div className="form-outline mb-4">
+                          <input
+                            type="email"
+                            id="form2Example11"
+                            className="form-control"
+                            placeholder="Phone number or email address"
+                          />
+                          <label
+                            className="form-label"
+                            htmlFor="form2Example11"
+                          >
+                            Username
+                          </label>
+                        </div>
+                        <div className="form-outline mb-4">
+                          <input
+                            type="password"
+                            id="form2Example22"
+                            className="form-control"
+                          />
+                          <label
+                            className="form-label"
+                            htmlFor="form2Example22"
+                          >
+                            Password
+                          </label>
+                        </div>
+                        <div className="text-center pt-1 mb-5 pb-1">
+                          <button
+                            className="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3"
+                            type="button"
+                          >
+                            Log in
+                          </button>
+                          <a className="text-muted" href="#!">
+                            Forgot password?
+                          </a>
+                        </div>
+                        <div className="d-flex align-items-center justify-content-center pb-4">
+                          <p className="mb-0 me-2">Don't have an account?</p>
+                          <button
+                            type="button"
+                            className="btn btn-outline-danger"
+                          >
+                            Create new
+                          </button>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                  <div className="col-lg-6 d-flex align-items-center gradient-custom-2">
+                    <div className="text-white px-3 py-4 p-md-5 mx-md-4">
+                      <h4 className="mb-4">We are more than just a company</h4>
+                      <p className="small mb-0">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit, sed do eiusmod tempor incididunt ut labore et
+                        dolore magna aliqua. Ut enim ad minim veniam, quis
+                        nostrud exercitation ullamco laboris nisi ut aliquip ex
+                        ea commodo consequat.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Login;
