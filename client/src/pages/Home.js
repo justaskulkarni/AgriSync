e2febb2f9f94d1ccaaad41a391e366dc2dacc1cc
AadiVj18 <vijayvargiya.meet@gmail.com>
2023-07-16 01:54:52 +0530

    changed bg and css

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "../stylesheets/Home.css";
+import logo from "../images/logo (2).jpeg";
 
 const Home = () => {
   return (
     <>
       {/* Navigation */}
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+      <nav className="navbar navbar-expand-lg bg-my static-top">
         <div className="container">
           <a className="navbar-brand" href="#">
-            <img
-              src="https://placeholder.pics/svg/150x50/888888/EEE/Logo"
-              alt="..."
-              height={36}
-            />
+            <div className="d-flex">
+              <img src={logo} alt="..." height={36} />{" "}
+              <p className="mx-3 my-1 fw-bold">Let's Endorse</p>
+            </div>
           </a>
           <button
             className="navbar-toggler"
@@ -28,97 +28,64 @@ const Home = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Home
+                <a className="nav-link fw-bold" aria-current="page" href="#">
+                  About us
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Link
+                <a className="nav-link fw-bold" href="#">
+                  Contact Us
                 </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul
-                  className="dropdown-menu dropdown-menu-end"
-                  aria-labelledby="navbarDropdown"
-                >
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
               </li>
             </ul>
           </div>
         </div>
       </nav>
 
-      {/* Hero */}
-      <div className="container">
-        <div className="row d-flex justify-content-between my-5">
-          <div className="p-5 text-left bg-14 col-md-4">
-            <h1 className="mb-3">Farmer</h1>
-            <h4 className="mb-3">Subheading</h4>
-            <a className="btn btn-primary" href role="button">
-              Call to action
-            </a>
-          </div>
-          <div className="p-5 text-left bg-23 col-md-4">
-            <h1 className="mb-3">MFE</h1>
-            <h4 className="mb-3">Subheading</h4>
-            <a className="btn btn-primary" href role="button">
-              Call to action
-            </a>
-          </div>
-        </div>
-      </div>
-
-      {/* second container for cards */}
-
-      <div className="container">
-        <div className="row d-flex justify-content-between  my-5">
-          <div className="p-5 text-left bg-23 col-md-4">
-            <h1 className="mb-3">PAC</h1>
-            <h4 className="mb-3">Subheading</h4>
-            <a className="btn btn-primary" href role="button">
-              Call to action
-            </a>
-          </div>
-          <div className="p-5 text-left bg-14 col-md-4">
-            <h1 className="mb-3">CPC</h1>
-            <h4 className="mb-3">Subheading</h4>
-            <a className="btn btn-primary" href role="button">
-              Call to action
-            </a>
+      <div className="bg-img">
+        {/* Hero */}
+        <div className="container">
+          <div className="row d-flex justify-content-between my-5">
+            <div className="p-5 text-left bg-14 col-md-4">
+              <h1 className="mb-3">Farmer</h1>
+              <h4 className="mb-3">Click to Login or Register</h4>
+              <a className="btn btn-primary" href role="button">
+                Call to action
+              </a>
+            </div>
+            <div className="p-5 text-left bg-23 col-md-4">
+              <h1 className="mb-3">MFE</h1>
+              <h4 className="mb-3">Click to Login or Register</h4>
+              <a className="btn btn-primary" href role="button">
+                Call to action
+              </a>
+            </div>
           </div>
         </div>
-      </div>
 
-      {/* Hero */}
+        {/* second container for cards */}
+
+        <div className="container">
+          <div className="row d-flex justify-content-between  my-5">
+            <div className="p-5 text-left bg-23 col-md-4">
+              <h1 className="mb-3">PAC</h1>
+              <h4 className="mb-3">Click to Login or Register</h4>
+              <a className="btn btn-primary" href role="button">
+                Call to action
+              </a>
+            </div>
+            <div className="p-5 text-left bg-14 col-md-4">
+              <h1 className="mb-3">CPC</h1>
+              <h4 className="mb-3">Click to Login or Register</h4>
+              <a className="btn btn-primary" href role="button">
+                Call to action
+              </a>
+            </div>
+          </div>
+        </div>
+
+        {/* Hero */}
+      </div>
     </>
   );
 };
